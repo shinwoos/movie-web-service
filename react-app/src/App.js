2af4b3e660320d4394739c6e4470a6741993ac5d
shinwoos <kmnj2100@gmail.com>
2024-03-22 04:25:54 +0900

    update commit movie project

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,48 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          {movies.map((item) => {
+            return (
+              <div key={item.id}>
+                <img src={item.medium_cover_image} />
+                <h2>{item.title}</h2>
+                <p>{item.summary}</p>
+                <ul>
+                  {item.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
